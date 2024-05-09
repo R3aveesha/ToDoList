158d2e2886beef4ce8d13c3e84b0614273434fac
raveesha <it22271150@my.sliit.lk>
2024-05-09 13:02:05 +0530

    updated

---
+++ toDolist/src/Home.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import Create from './create'
+
+
+function Home() {
+    const [todos,setTodos]=useState([])
+
+  return (
+    <div>
+      <h2>Todo List</h2>
+      <Create/>
+      {
+        todos.length===0
+        ?
+        <div><h2>No records</h2></div>
+        :
+        todos.map(todo=>(
+            <div>
+                {todo}
+            </div>
+        ))
+      }
+      
+    </div>
+  )
+}
+
+export default Home
