26843ef5f351ae4b37cf7ed3d8aecd31a3cebfc9
raveesha <it22271150@my.sliit.lk>
2024-05-16 13:12:54 +0530

    updated

--- Server/Models/todo.js
+++ Server/Models/todo.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-const todoSchema = new mongoose.Schema({
+const TodoSchema = new mongoose.Schema({
     task: {
         type: String,
         required: true
     }
 });
 
-const Todo = mongoose.model('Todo', todoSchema);
+const Todo = mongoose.model('Todo', TodoSchema);
 
 module.exports = Todo;
